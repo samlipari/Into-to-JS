286afa6020a07ddb34414dcbfe68138fd88a07af
samlipari <70296400+samlipari@users.noreply.github.com>
2020-10-29 18:27:46 -0400

    Create var-four.js

---
+++ variables/var-four.js
@@ -0,0 +1,11 @@
+const NAME = 'Victor';
+console.log('Good Morning, ' + NAME);
+console.log('Good Afternoon, ' + NAME);
+console.log('Good Evening, ' + NAME);
+
+NAME = 'Joe';
+console.log('Good Morning, ' + NAME);
+console.log('Good Afternoon, ' + NAME);
+console.log('Good Evening, ' + NAME);
+
+returns an error because you can't reassign a const variable
