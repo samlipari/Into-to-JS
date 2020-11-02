79f0a09066ab15dd8bd2b7ef2f7e454cd3dad568
samlipari <70296400+samlipari@users.noreply.github.com>
2020-11-02 11:56:11 -0500

    Create function-six.js

---
+++ functions/function-six.js
@@ -0,0 +1,7 @@
+function scream(words) {
+  return words + '!!!!';
+}
+
+scream('Yipeee');
+
+should log Yipeee!!!! - wrong, does not log anything b/c it doesn't log anything to console just returns a value.
