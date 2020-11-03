3bf8ad221b79ad7c8ec93d0ad6d0a535dc62e5be
samlipari <70296400+samlipari@users.noreply.github.com>
2020-11-03 10:19:14 -0500

    Update function-three.js

--- functions/function-three.js
+++ functions/function-three.js
@@ -1,12 +1,8 @@
-function multiply(left, right) {
-  return left * right;
-}
+  function multiply(num1, num2) {
+    return num1 * num2;
+  }
 
-function getNumber(prompt) {
-  let readlineSync = require('readline-sync');
-  return parseFloat(readlineSync.question(prompt));
-}
-
-let left = getNumber('Enter the first number: ');
-let right = getNumber('Enter the second number: ');
-console.log(`${left} * ${right} = ${multiply(left, right)}`);
+  let n1 = prompt('Enter the first number:')
+  let n2 = prompt('Enter the second number:');
+  let product = multiply(n1, n2);
+  console.log(`${n1} * ${n2} = ${product}`);
